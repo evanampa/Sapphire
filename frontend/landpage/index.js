8dabd3d7ac24b0a4f4ba66508ae2235144d59d5b
evanampa <evanampa@csd.auth.gr>
2025-05-24 14:13:20 +0300

    Modified landpage, added responsive design

---
+++ frontend/landpage/index.js
@@ -0,0 +1,10 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+
+menuOpenButton.addEventListener("click", () => {
+  //Toggle menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
