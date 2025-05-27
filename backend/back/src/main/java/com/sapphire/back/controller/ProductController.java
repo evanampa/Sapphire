a306b3ad69a48349826ae03e2dbd07c369d76565
evanampa <evanampa@csd.auth.gr>
2025-05-27 18:32:58 +0300

    modified products-section, now they appear dynamically

--- backend/back/src/main/java/com/sapphire/back/controller/ProductController.java
+++ backend/back/src/main/java/com/sapphire/back/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.sapphire.back.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,15 @@ import com.sapphire.back.service.ProductService;
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin(origins = "http://127.0.0.1:5500") 
 public class ProductController {
 
   @Autowired
   ProductService productService;
 
-
   @GetMapping("")
-  public List<Product> getProducts(){
-    return productService.getAllProducts();
+  public List<Product> getProducts() {
+      return productService.getAllProducts();
   }
 
   /* 
--- frontend/landpage/index.js
+++ frontend/landpage/index.js
@@ -12,3 +12,31 @@ menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 navLinks.forEach( link => {
   link.addEventListener("click", () => menuOpenButton.click());
 });
+
+// Using the fetch API to call an example endpoint
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:8080/products")
+    .then((response) => response.json())
+    .then((products) => {
+      const productList = document.querySelector(".product-list");
+
+      productList.innerHTML = ""; // Καθαρίζουμε ό,τι είχε
+
+      products.forEach((product) => {
+        productList.innerHTML += `
+          <li class="product-item">
+            <img
+              src="${product.image_url}"
+              alt="${product.name}"
+              class="product-image"
+            />
+            <h3 class="name">${product.name}</h3>
+            <p class="text">${product.description}</p>
+          </li>
+        `;
+      });
+    })
+    .catch((error) => {
+      console.error("Error loading products:", error);
+    });
+});
